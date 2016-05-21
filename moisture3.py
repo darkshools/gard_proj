f235623cbf43dae930cd30131f0e66350ec350d6
darkshools <darkshools@gmail.com>
2016-05-21 18:00:11 +0100

    Main code for controlling my garden pump

---
+++ moisture3.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+# Start by importing the libraries we want to use
+
+import RPi.GPIO as GPIO # This is the GPIO library we need to use the GPIO pins on the Raspberry Pi
+import time # This is the time library, we need this so we can use the sleep function
+
+# Define some variables to be used later on in our script
+
+
+# This is our callback function, this function will be called every time there is a change on the specified GPIO channel, in this example we are using 17
+
+def callback(sensor):  
+	if GPIO.input(sensor):	
+		GPIO.output(pump, GPIO.HIGH)
+		print "Dry"
+	else:
+		GPIO.output(pump, GPIO.LOW)
+		print "Water"
+
+# Set our GPIO numbering to BCM
+GPIO.setmode(GPIO.BCM)
+
+# Define the GPIO pin that we have our digital output from our sensor connected to
+sensor = 17
+pump = 22
+
+# Set the GPIO pin to an input
+GPIO.setup(sensor, GPIO.IN)
+GPIO.setup(pump, GPIO.OUT)
+
+# This line tells our script to keep an eye on our gpio pin and let us know when the pin goes HIGH or LOW
+GPIO.add_event_detect(sensor, GPIO.BOTH, bouncetime=300)
+# This line asigns a function to the GPIO pin so that when the above line tells us there is a change on the pin, run this function
+GPIO.add_event_callback(sensor, callback)
+
+# This is an infinte loop to keep our script running
+while True:
+	# This line simply tells our script to wait 0.1 of a second, this is so the script doesnt hog all of the CPU
+	time.sleep(0.1)
